1afaf631b05f4cee5174509045eecb499b5b5f73
DP <davidw@vosgeschocolate.com>
2012-10-31 13:08:33 -0500

    2nd commit test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RainsinstallerDemo::Application.config.secret_token = 'f4a93d6fdff890119c9880172a347ca8474bc25d99ffe2d03534f94c737f878ba309d1117aeedecb8b383a8708117e2ec28a33b838f04d5ef69d77a320c389a7'
